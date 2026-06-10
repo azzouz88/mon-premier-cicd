75660695fbc7502fd7111e7aa1f5e219c6769235
azzouz <tabbiza@gmail.com>
2026-06-10 22:36:56 +0200

    ajout de la fct soustraction

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st
-from calcul import ajouter
+from calcul import ajouter, soustraire
 
 st.title("🧮 Ma super calculatrice automatisée")
 st.write("Bienvenue sur mon application déployée en CI/CD !")
 
-# Formulaire pour la calculatrice
 a = st.number_input("Entrez le premier nombre", value=0)
 b = st.number_input("Entrez le deuxième nombre", value=0)
 
-if st.button("Calculer l'addition"):
-    resultat = ajouter(a, b)
-    st.success(f"Le résultat est : {resultat}")
+# On crée deux colonnes pour aligner les boutons
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Calculer l'addition"):
+        st.success(f"Le résultat est : {ajouter(a, b)}")
+
+with col2:
+    if st.button("Calculer la soustraction"):
+        st.info(f"Le résultat est : {soustraire(a, b)}")
