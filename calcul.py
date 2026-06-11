d79363ef71cac6af5183f19c6f5138600b74252d
azzouz <tabbiza@gmail.com>
2026-06-11 17:13:34 +0200

    Fix: correction de la fct div pour le linter Flake8

--- calcul.py
+++ calcul.py
@@ -4,5 +4,6 @@ def ajouter(a, b):
 def soustraire(a, b):
     return a - b  # <-- Et un "-" ici !
 def diviser(a, b):
-    # Volontairement buggé : 'variable_inconnue' n'existe nulle part !
-    return a / variable_inconnue
+    if b == 0:
+        return "Erreur : Division par zéro"
+    return a / b
